2e5ee81ba5b48c20dcc95e04dc77551c6b1f3658
Eduard <e.keilholz@4dotnet.nl>
2022-06-13 11:17:33 +0200

    git commit

---
+++ src/HexMaster.SimpleApi/Controllers/BlobsController.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HexMaster.SimpleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlobsController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var identity = new DefaultAzureCredential(true);
+//            var token = await identity.GetTokenAsync(new TokenRequestContext(new[]{"https://simplemsidemo.blob.core.windows.net"}), CancellationToken.None);
+//            var tokenCredential = TokenAcquisitionTokenCredential
+            var blobContainerClient = new BlobContainerClient(new Uri("https://simplemsidemo.blob.core.windows.net/images"), identity);
+            var blobs = blobContainerClient.GetBlobs();
+            foreach (var page in blobs.AsPages())
+            {
+                foreach (var value in page.Values)
+                {
+                    Console.WriteLine(value);
+                }
+            }
+            var blobClient =  blobContainerClient.GetBlobClient("works-on-my-machine.jpg");
+            var blob = await blobClient.DownloadContentAsync();
+            return new FileContentResult(blob.Value.Content.ToArray(), "images/png");
+        }
+    }
+}
---
+++ src/HexMaster.SimpleApi/Controllers/SecretsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HexMaster.SimpleApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SecretsController : ControllerBase
+    {
+        private readonly IConfiguration _config;
+
+        [HttpGet]
+        public IActionResult GetConfigurationValue()
+        {
+            return Ok(_config["SecretValue"]);
+        }
+
+        public SecretsController(IConfiguration config)
+        {
+            _config = config;
+        }
+    }
+}
